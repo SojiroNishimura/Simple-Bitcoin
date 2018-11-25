98e36f6142a7f1f6e9811cbc83bde17b7759c154
Sojiro Nishimura <sojiro.nishimura@japan-d2.com>
2018-11-25 20:15:41 +0900

    Add Blockchain validation

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,30 +1,68 @@
 import json
+import hashlib
+import binascii
+
 from time import time
+from datetime import datetime
 
 
 class Block:
-    def __init__(self, transaction, previous_block_hash):
+    def __init__(self, transactions, previous_block_hash):
+        snap_tr = json.dumps(transactions) # Pow計算中に値が変わるのでブロック計算開始時の値を退避する
+
         self.timestamp = time()
-        self.transaction = transaction
+        self.transactions = json.loads(snap_tr)
         self.previous_block = previous_block_hash
 
-    def to_dict(self):
+        current = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        print(current)
+
+        json_block = json.dumps(self.to_dict(include_nonce=False), sort_keys=True)
+        print('json_block: ', json_block)
+        self.nonce = self._compute_nonce_for_pow(json_block)
+
+        current2 = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        print(current2)
+
+    def to_dict(self, include_nonce=True):
         d = {
             'timestamp': self.timestamp,
-            'transaction': json.dumps(self.transaction),
+            'transactions': json.dumps(self.transactions),
             'previous_block': self.previous_block,
         }
+
+        if include_nonce:
+            d['nonce'] = self.nonce
+
         return d
+
+    def _compute_nonce_for_pow(self, message, difficulty=5):
+        i = 0
+        suffix = '0' * difficulty
+        while True:
+            nonce = str(i) # 0から総当たりで数を増やしてsuffixを満たす値を探す
+            digest = binascii.hexlify(self._get_double_sha256((message + nonce).encode('utf-8'))).decode('ascii')
+
+            if digest.endswith(suffix):
+                return nonce
+            i += 1
+
+    def _get_double_sha256(self, message):
+        return hashlib.sha256(hashlib.sha256(message).digest()).digest()
 
 
 class GenesisBlock(Block):
     def __init__(self):
-        super().__init__(transaction='AD9B477B42B22CDF18B1335603D07378ACE83561D8398FBFC8DE94196C65D806', previous_block_hash=None)
+        super().__init__(transactions='AD9B477B42B22CDF18B1335603D07378ACE83561D8398FBFC8DE94196C65D806', previous_block_hash=None)
 
-    def to_dict(self):
+    def to_dict(self, include_nonce=True):
         d = {
-            'transaction': self.transaction,
+            'transactions': self.transactions,
             'genesis_block': True,
         }
+
+        if include_nonce:
+            d['nonce'] = self.nonce
+
         return d
 
--- blockchain/blockchain_manager.py
+++ blockchain/blockchain_manager.py
@@ -2,6 +2,8 @@ import json
 import hashlib
 import binascii
 import threading
+import copy
+import pickle
 
 
 class BlockchainManager:
@@ -18,6 +20,49 @@ class BlockchainManager:
     def set_new_block(self, block):
         with self.lock:
             self.chain.append(block)
+
+    def is_valid_block(self, prev_block_hash, block, difficulty=3):
+        suffix = '0' * difficulty
+        block_4_pow = copy.deepcopy(block)
+        nonce = block_4_pow['nonce']
+        del block_4_pow['nonce']
+        print(block_4_pow)
+
+        message = json.dumps(block_4_pow, sort_keys=True)
+        nonce = str(nonce)
+
+        if block['previous_block'] != prev_block_hash:
+            print('Invalid block (bad previous_block)')
+            print(block['previous_block'])
+            print(prev_block_hash)
+            return False
+        else:
+            digest = binascii.hexlify(self._get_double_sha256((message + nonce)
+                .encode('utf-8'))).decode('ascii')
+            if digest.endswith(suffix):
+                print('OK, this seems valid block')
+                return True
+            else:
+                print('Invalid block (bad nonce)')
+                print('nonce: ', nonce)
+                print('digest: ', digest)
+                print('suffix: ', suffix)
+                return False
+    
+    def is_valid_chain(self, chain):
+        # チェーン全体の正当性を検証する
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            if self.is_valid_block(self.get_hash(last_block), block) is not True:
+                return False
+
+            last_block = chain[current_index]
+            current_index += 1
+
+        return True
 
     def is_valid(self, chain):
         last_block = chain[0]
@@ -39,3 +84,84 @@ class BlockchainManager:
     def get_hash(self, block):
         block_string = json.dumps(block, sort_keys=True)
         return binascii.hexlify(self._get_double_sha256((block_string).encode('utf-8'))).decode('ascii')
+
+    def get_transactions_from_orphan_blocks(self, orphan_blocks):
+        current_index = 1
+        new_transactions = []
+
+        while current_index < len(orphan_blocks):
+            block = orphan_blocks[current_index]
+            transactions = block['transactions']
+            target = self.remove_useless_transaction(transactions)
+            for t in target:
+                new_transactions.append(t)
+
+        return new_transactions
+
+    def get_my_blockchain(self):
+        if len(self.chain) > 1:
+            return self.chain
+        else:
+            return None
+
+    def remove_useless_transaction(self, transaction_pool):
+        """
+        与えられたTransactionのリストに、すでに自分が管理するメインチェーンに含まれるTransactionがある場合、
+        そのTransactionを削除したTransactionのリストを返す
+        """
+
+        if len(transaction_pool) != 0:
+            current_index = 1
+
+            while current_index < len(self.chain):
+                block = self.chain[current_index]
+                transactions = block['transactions']
+                for t in transactions:
+                    for t2 in transaction_pool:
+                        if t == json.dumps(t2):
+                            print('already exists in my blockchain : ', t2)
+                            transaction_pool.remove(t2)
+                current_index += 1
+
+            return transaction_pool
+        else:
+            print('no transaction to be removed...')
+            return []
+
+    def resolve_conflicts(self, chain):
+        # 自分のブロックチェーンと比較して長い方を有効とす
+        mychain_len = len(self.chain)
+        new_chain_len = len(chain)
+
+        pool_4_orphan_blocks = copy.deepcopy(self.chain)
+        has_orphan = False
+
+        # 自分のチェーンの中でだけ処理済みとなっているTransactionを救出する
+        # 現在のチェーンに含まれていないブロックを全て取り出す。
+        # 時系列を考えての有効無効判定などはしない簡易処理
+        if new_chain_len > mychain_len:
+            for b in pool_4_orphan_blocks:
+                for b2 in chain:
+                    if b == b2:
+                        pool_4_orphan_blocks.remove(b)
+
+            result = self.renew_my_blockchain(chain)
+            print(result)
+            if result is not None:
+                return result, pool_4_orphan_blocks
+            else:
+                return None, []
+        else:
+            print('invalid chain cannot be set...')
+            return None, []
+
+    def renew_my_blockchain(self, blockchain):
+        # ブロックチェーン自体を更新し、それによって変更されるはずの最新のprev_block_hashを計算して返却する
+        with self.lock:
+            if self.is_valid_chain(blockchain):
+                self.chain = blockchain
+                latest_block = self.chain[-1]
+                return self.get_hash(latest_block)
+            else:
+                print('invalid chain cannot be set...')
+                return None
